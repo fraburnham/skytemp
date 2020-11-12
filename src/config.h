16447f3ed73b2c7328bf35263a8623139ed5ed69
Frank Burnham <frank@n8git.dev>
2020-11-12 09:08:40 -0500

    Use adafruit libs for dht

---
+++ src/config.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define DHT_PIN 5
+#define DHT_TYPE DHT11
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
 #include "Arduino.h"
+#include "DHT.h"
 #include "wifi.h"
+#include "config.h"
+
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(10);
   Serial.println("\nConnecting WiFi...");
   Serial.print("Success: ");
   Serial.println(initWiFi());
 }
 
+// need to get away from loop into interrupts for lower power consumption
 void loop()
 {
-  delay(1000);
+  delay(10000);
+
+  float relative_humidity = dht.readHumidity();
+  float temp_C = dht.readTemperature();
+
+  Serial.print("Relative Humidity: ");
+  Serial.print(relative_humidity);
+  Serial.println("%");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp_C);
+  Serial.println("C");
 }
